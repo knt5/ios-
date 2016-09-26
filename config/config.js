2b354db971604e8b8e916070f0a7fde6d250bc5e
knt5 <knt5@users.noreply.github.com>
2016-09-27 01:20:23 +0900

    Refactor config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,8 @@
+const home = process.env['HOME'];
+const cwd = process.cwd();
+
 module.exports = {
-	photoLibraryDir: process.env['HOME'] + '/0/photo/photo-library',
-	//photoLibraryDir: process.env['HOME'] + '/Desktop/photo/photo-library-test',
-	workDir: process.cwd() + '/data'
+	photoLibraryDir: home + '/0/photo/photo-library',
+	//photoLibraryDir: home + '/Desktop/photo/photo-library-test',
+	workDir: cwd + '/data'
 };
