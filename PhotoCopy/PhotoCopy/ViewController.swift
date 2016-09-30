97d9d94e193638879c3381f4d1a0357e25336431
knt5 <knt5@users.noreply.github.com>
2016-09-30 23:20:53 +0900

    Add an iOS app to copy media data to photos

---
+++ PhotoCopy/PhotoCopy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoCopy
+//
+//  Created by apple on 2016/09/30.
+//  Copyright © 2016年 Kenta Motomura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	@IBOutlet weak var copyButton: UIButton!
+	@IBOutlet weak var progressLabel: UILabel!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+
+
+}
+
