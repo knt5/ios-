029c2fab33a67cd74b736b557a60837ec3feb10f
knt5 <knt5@users.noreply.github.com>
2018-01-05 12:40:57 +0900

    Update startNumber

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
-const home = process.env['HOME'];
+ const home = process.env['HOME'];
 const cwd = process.cwd();
 
 module.exports = {
 	photoLibraryDir: home + '/0/photo/photo-library',
-	startNumber: 50595,
+	startNumber: 51167,
 	workDir: cwd + '/data'
 };
